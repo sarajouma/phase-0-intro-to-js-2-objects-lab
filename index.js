3176307759d8e6dd4941b813764ee201264504f5
sarajouma <144836947+sarajouma@users.noreply.github.com>
2023-10-01 22:08:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name : "SafeArray",
+    streetAddress :"11"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newemployee = { ...employee };
+    newemployee[key] = value;
+    return newemployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newemployee = { ...employee };
+    delete newemployee[key] ;
+    return newemployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
